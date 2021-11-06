3295a3633dff32bdd08d37b35f2ad111d0e13b55
DianaKuo <cj0102520@gmail.com>
2021-11-07 00:39:23 +0800

    付款成功寄信ok

--- src/main/java/com/Gather/Sponsorship/controller/SponsorshipPageController.java
+++ src/main/java/com/Gather/Sponsorship/controller/SponsorshipPageController.java
@@ -26,6 +26,7 @@ import com.Gather.Sponsorship.model.SponsorOrderBean;
 import com.Gather.Sponsorship.service.SponsorOrderService;
 import com.Gather.member.entity.Member;
 import com.Gather.member.service.MemberService;
+import com.Gather.util.Mail;
 
 import ecpay.payment.integration.AllInOne;
 import ecpay.payment.integration.domain.AioCheckOutALL;
@@ -54,105 +55,57 @@ public class SponsorshipPageController {
 		return "Sponsorship/favorite";
 	}
 
-//	//跳轉付款頁面
-//	@PostMapping("/goECPay")
-//	@ResponseBody
-//	public  String genAioCheckOutALL(HttpServletRequest request,
-//			@RequestParam("sBonus") String sBonus,
-//			@RequestParam("sAmount") String sAmount,
-//			@RequestParam("sDiscount") String sDiscount,
-//			@RequestParam("sID") String sID
-//			) {
-//
-//
-//		SponsorOrderBean sob =sponsorOrderService.getOrderBySponsorshipID((sID));
-//		System.out.println(sob.getsBonus());
-//		Integer sTotal = (sAmount) - (sDiscount) + (sBonus);
-//		
-//		String MerchantTradeDate=sob.getsTime().replace("-", "/");
-//		
-//		String tradeNo = "item" + sob.getsID();
-//		System.out.println(tradeNo);
-//		System.out.println(sob.getsID());
-//		
-//		AllInOne all = new AllInOne("");
-//		
-//		AioCheckOutALL obj = new AioCheckOutALL();
-//		
-//		
-//		obj.setMerchantID("2000132");  
-//		obj.setMerchantTradeDate(MerchantTradeDate);
-//		obj.setTotalAmount(sTotal.toString());
-//		obj.setTradeDesc(sob.getmID() + "-" +sob.getsID() +  "-" + sob.getsTime());
-//		obj.setItemName("Gather贊助一筆");
-//		obj.setMerchantTradeNo(tradeNo);
-//		obj.setReturnURL("http://211.23.128.214:5000");
-//		obj.setNeedExtraPaidInfo("N");
-//		obj.setClientBackURL("/");
-//		String form = all.aioCheckOut(obj, null);
-//		return form;			
-//		
-//		
-//	}
-
 	// 跳轉付款頁面
 	@PostMapping("/goECPay")
-		@ResponseBody
-		public  String genAioCheckOutALL(HttpServletRequest request,
-				@RequestParam("sPID") String sPID, @RequestParam("sPName") String sPName,
-				@RequestParam("projectImage") String projectImage, @RequestParam("mID") String mID,
-				@RequestParam("sName") String sName, @RequestParam("sPhone") String sPhone,
-				@RequestParam("sBonus") String sBonus, @RequestParam("sAddress") String sAddress,
-				@RequestParam("sEmail") String sEmail, @RequestParam("sAmount") String sAmount,
-				@RequestParam("sDiscount") String sDiscount, @RequestParam("paymentMethod") String paymentMethod,
-				@RequestParam("proposerID") String proposerID
-	
-				) {
-			//新增訂單
-			
+	@ResponseBody
+	public String genAioCheckOutALL(HttpServletRequest request, @RequestParam("sPID") String sPID,
+			@RequestParam("sPName") String sPName, @RequestParam("projectImage") String projectImage,
+			@RequestParam("mID") String mID, @RequestParam("sName") String sName, @RequestParam("sPhone") String sPhone,
+			@RequestParam("sBonus") String sBonus, @RequestParam("sAddress") String sAddress,
+			@RequestParam("sEmail") String sEmail, @RequestParam("sAmount") String sAmount,
+			@RequestParam("sDiscount") String sDiscount, @RequestParam("paymentMethod") String paymentMethod,
+			@RequestParam("proposerID") String proposerID
 
-				Integer sTotal =Integer.parseInt(sAmount)-Integer.parseInt(sDiscount)+Integer.parseInt(sBonus);
-				SponsorOrderBean sBean = new SponsorOrderBean(Integer.parseInt(mID), Integer.parseInt(sPID), sName, sPName,
-						paymentMethod, Integer.parseInt(sAmount), Integer.parseInt(sDiscount), Integer.parseInt(sBonus),
-						projectImage, sAddress, sPhone, sEmail, sTotal, Integer.parseInt(proposerID));
+	) {
+		// 新增訂單
+		Integer sTotal = Integer.parseInt(sAmount) - Integer.parseInt(sDiscount) + Integer.parseInt(sBonus);
+		SponsorOrderBean sBean = new SponsorOrderBean(Integer.parseInt(mID), Integer.parseInt(sPID), sName, sPName,
+				paymentMethod, Integer.parseInt(sAmount), Integer.parseInt(sDiscount), Integer.parseInt(sBonus),
+				projectImage, sAddress, sPhone, sEmail, sTotal, Integer.parseInt(proposerID));
 
-				Long timeStamp = System.currentTimeMillis();
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-				String sd = sdf.format(new Date(Long.parseLong(String.valueOf(timeStamp))));
+		Long timeStamp = System.currentTimeMillis();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String sd = sdf.format(new Date(Long.parseLong(String.valueOf(timeStamp))));
 
-				sBean.setsTime(sd);
-				sBean.setStatus("已付款");
-				sponsorOrderService.insertOrder(sBean);
-//				model.addAttribute("sID",sBean.getsID()) ;
-			
+		sBean.setsTime(sd);
+		sBean.setStatus("已付款");
+		sponsorOrderService.insertOrder(sBean);
+
+
+		String tradeNo = "Gather" + sBean.getsID();
+		System.out.println(tradeNo);
+		System.out.println(sBean.getsID());
+
+		AllInOne all = new AllInOne("");
+
+		AioCheckOutALL obj = new AioCheckOutALL();
+
+		obj.setMerchantID("2000132");
+		obj.setMerchantTradeDate(sd);
+		obj.setTotalAmount(sTotal.toString());
+		obj.setTradeDesc(sBean.getmID() + "-" + sBean.getsID() + "-" + sBean.getsTime());
+		obj.setItemName("Gather贊助一筆");
+		obj.setMerchantTradeNo(tradeNo);
+		obj.setReturnURL("http://211.23.128.214:5000");
+		obj.setNeedExtraPaidInfo("N");
+		obj.setClientBackURL("http://localhost:8080/Gather");
+		String form = all.aioCheckOut(obj, null);
 		
-//			Integer sTotal = (sAmount) - (sDiscount) + (sBonus);
-			
-			
-	
-			
-			String tradeNo = "Gather" + sBean.getsID();
-			System.out.println(tradeNo);
-			System.out.println(sBean.getsID());
-			
-			AllInOne all = new AllInOne("");
-			
-			AioCheckOutALL obj = new AioCheckOutALL();
-			
-			
-			obj.setMerchantID("2000132");  
-			obj.setMerchantTradeDate(sd);
-			obj.setTotalAmount(sTotal.toString());
-			obj.setTradeDesc(sBean.getmID() + "-" +sBean.getsID() +  "-" + sBean.getsTime());
-			obj.setItemName("Gather贊助一筆");
-			obj.setMerchantTradeNo(tradeNo);
-			obj.setReturnURL("http://211.23.128.214:5000");
-			obj.setNeedExtraPaidInfo("N");
-			obj.setClientBackURL("http://localhost:8080/Gather");
-			String form = all.aioCheckOut(obj, null);
-			return form;			
-			
-			
-		}
+		Member mBean=memberService.getMemberInfoByID(Integer.parseInt(mID));
+		Mail.SendGmail("Gather.WebService@gmail.com",mBean.getAccount(),"Gather贊助平台付款成功通知", mBean.getName()+"您好!\n"+"您的訂單"+tradeNo.toString()+"已成功贊助"+sPName+"專案，贊助總額為"+sTotal+"元\n"+"非常感謝您對本平台的喜愛，再次感謝您的贊助!");
+		System.out.println("成功寄信");
+		return form;
+
+	}
 
 }
