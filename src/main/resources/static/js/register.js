2d4638fe1793176bb828aeab0c6b07da1e53b063
wdiw <58246835+wdiw@users.noreply.github.com>
2021-11-12 16:38:44 +0800

    導入 jq-validattion外掛，配置註冊頁面成功。 若沒有符合validate 條件，無法進行註冊。

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,135 +1,130 @@
 $(function () {
-
-
-
-    /*
-    setTimeout(function(){
-        $("#div2").hide()
-        //跳轉到信箱認證頁面
-          $(location).attr('./mail.html');
-       }, 1500)
-       setTimeout();
+    //驗證
+    //  $("button").mousedown(function () {
+    //     $("p").slideToggle();
+    // });
+    $('#f').validate({
+        /* 常用檢測屬性
+       required:必填
+       noSpace:空白
+       minlength:最小長度
+       maxlength:最大長度
+       email:信箱格式
+       number:數字格式
+       url:網址格式https://www.minwt.com
        */
-    //新增
-    $("#add").click(function () {
-        let user_name = $('#text_name').val();
-        let user_account = $("#text_account").val();
-        let user_password = $("#text_password").val();
-        let user_birthday = $("#text_birthday").val();
-        let user_address = $("#text_address").val();
-        let user_phone = $("#text_phone").val();
-        if (document.getElementsByName('sex')[0].checked) {
-            var user_sexual = '男';
-        } else {
-            var user_sexual = '女';
-        }
-
-        console.log(
-            "姓名" +
-            user_name + "帳號" +
-            user_account + "密碼" +
-            user_password + "生日" +
-            user_birthday + "地址" +
-            user_address + "性別" +
-            user_sexual + "電話" +
-            user_phone
-            //ok
-        )
-
-        if ($.trim(user_name) == '' |
-            $.trim(user_account) == '' |
-            $.trim(user_password) == '' |
-            $.trim(user_address) == '' |
-            $.trim(user_birthday) == '' |
-            $.trim(user_phone) == ''
-        ) {
-
-            Swal.fire({
-                icon: 'error',
-                position: 'center',
-                title: '全為必填，請重新檢查表單'   
-            })
-            (this).focus;
-            return;
-        }
-
-
-        var params = {
-            "name": '' + user_name,
-            "account": '' + user_account,
-            "password": '' + user_password,
-            "address": '' + user_address,
-            "sexual": '' + user_sexual,
-            "birthday": '' + user_birthday,
-            "phone": '' + user_phone
-        };
-
-        $.ajax({
-            type: 'post',
-            url: 'http://localhost:8080/Gather/api/register',
-            contentType: 'application/json',
-            data: JSON.stringify(params),
-            success: function (data) {
-                Swal.fire({
-                    icon: 'success',
-                    position: 'center',
-                    title: '認證信已寄出',
-                })
-                setTimeout(function () {
-                    $("#div2").hide();
-                    //跳轉到信箱認證頁面
-                    $(location).attr('href', './mail.html');
-                }, 1500);
+        onkeyup: function (element, event) {
+            //去除左側空白
+            var value = this.elementValue(element).replace(/^\s+/g, "");
+            $(element).val(value);
+        },
+        rules: {
+            name: {
+                required: true
             },
-            error: function (e) {
-                console.log(e);
+            sex: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            pwd: {
+                required: true,
+                minlength: 6
+            },
+            phone: {
+                required: true,
+                minlength: 8,
+                number: true
+            },
+            address: 'required',
+            birthday: 'required',
+            checkbox_checked: 'required',
+            url: {
+                url: true
             }
-        });
-    });
 
+        },
+        messages: {
+            name: {
+                required: '必填'
+            },
+            sex:'必填',
+            birthday:'必填',
+            checkbox_checked:'請閱讀',
+            email: {
+                required: '必填',
+                email: 'Email格式不正確'
+            },
+            pwd: {
+                required: '必填',
+                minlength: '不得少於6位數'
+            },
+            phone: {
+                required: '必填',
+                minlength: '不得少於8位',
+                number: '電話需為數字'
+            },
+            address: '必填',
 
+            url: '網址格式不正確'
+        },
+        submitHandler: function (f) {
 
+            let user_name = $('#text_name').val();
+            let user_account = $("#text_account").val();
+            let user_password = $("#text_password").val();
+            let user_birthday = $("#text_birthday").val();
+            let user_address = $("#text_address").val();
+            let user_phone = $("#text_phone").val();
+            if (document.getElementsByName('sex')[0].checked) {
+                var user_sexual = '男';
+            } else {
+                var user_sexual = '女';
+            }
 
-    $("#valid").click(function () {
-        let code = $('#six_digit_code').val();
-        console.log(code) // ok
+            console.log(
+                "姓名" +
+                user_name + "帳號" +
+                user_account + "密碼" +
+                user_password + "生日" +
+                user_birthday + "地址" +
+                user_address + "性別" +
+                user_sexual + "電話" +
+                user_phone
+                //ok
+            )
 
-        if ($.trim(code) == '') {
-            alert('不得空白!!');
-            (this).focus;
-            return;
-        }
+            var params = {
+                "name": '' + user_name,
+                "account": '' + user_account,
+                "password": '' + user_password,
+                "address": '' + user_address,
+                "sexual": '' + user_sexual,
+                "birthday": '' + user_birthday,
+                "phone": '' + user_phone
+            };
 
-
-        $.ajax({
-            type: 'get',
-            url: '/Gather/api/register/' + code,
-            success: function (data) {
-                if (data == "Y") {
+            $.ajax({
+                type: 'post',
+                url: 'http://localhost:8080/Gather/api/register',
+                contentType: 'application/json',
+                data: JSON.stringify(params),
+                success: function (data) {
                     Swal.fire({
                         icon: 'success',
                         position: 'center',
-                        title: '驗證碼正確，成功註冊!!',
+                        title: '認證信已寄出',
                     })
                     setTimeout(function () {
                         $("#div2").hide();
                         //跳轉到信箱認證頁面
-                        $(location).attr('href', '/Gather');
+                        $(location).attr('href', './mail.html');
                     }, 1500);
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        position: 'center',
-                        title: '驗證碼錯誤',
-                    })
+                },
+                error: function (e) {
+                    console.log(e);
                 }
-
-
-            },
-            error: function () {
-                alert("連線失敗失敗")
-            }
-        })
-
-    })
+            });
+        }
+    });
 })
